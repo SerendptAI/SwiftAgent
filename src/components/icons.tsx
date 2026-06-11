30fcc85aaefdbfda1341446d21386fd89b97534c
romeoscript <romeobourne211@gmail.com>
2026-06-11 15:42:39 +0100

    style(icons): thin KeyRound icon stroke to 1.5

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -526,6 +526,7 @@ export const Icons = {
       </g>
     </svg>
   ),
+
   SettingsProfileWhite: (props: React.SVGProps<SVGSVGElement>) => (
     <svg
       {...props}
@@ -556,6 +557,23 @@ export const Icons = {
         strokeLinecap="round"
         strokeLinejoin="round"
       />
+    </svg>
+  ),
+  KeyRound: (props: React.SVGProps<SVGSVGElement>) => (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z" />
+      <circle cx="16.5" cy="7.5" r=".5" fill="currentColor" />
     </svg>
   ),
   SettingsCompany: (props: React.SVGProps<SVGSVGElement>) => (
